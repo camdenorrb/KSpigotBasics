9ce02b7cb81ec28f87536a7cc01b346dfd073ea3
camdenorrb <camdenorrb@users.noreply.github.com>
2017-11-21 19:49:41 -0600

    Even more Java???? O.o

--- src/main/kotlin/me/camdenorrb/kspigotbasics/ext/text.kt
+++ src/main/kotlin/me/camdenorrb/kspigotbasics/ext/text.kt
@@ -3,4 +3,5 @@ package me.camdenorrb.kspigotbasics.ext
 import org.bukkit.ChatColor.translateAlternateColorCodes
 
 
+@JvmOverloads
 fun String.color(char: Char = '&') = translateAlternateColorCodes(char, this)
--- src/main/kotlin/me/camdenorrb/kspigotbasics/gui/slot/ChestSlot.kt
+++ src/main/kotlin/me/camdenorrb/kspigotbasics/gui/slot/ChestSlot.kt
@@ -4,6 +4,7 @@ import org.bukkit.event.inventory.ClickType
 import org.bukkit.event.inventory.ClickType.LEFT
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.inventory.ItemStack
+import java.util.function.Consumer
 
 
 private typealias Action = InventoryClickEvent.() -> Unit
@@ -14,10 +15,18 @@ class ChestSlot internal constructor(val item: ItemStack, val slot: Int) {
 	val actions = mutableMapOf<ClickType, Action>()
 
 
+	@JvmSynthetic
 	fun addAction(clickType: ClickType = LEFT, action: Action)
 		= actions.put(clickType, action)
 
+	@JvmOverloads
 	fun remAction(clickType: ClickType = LEFT)
 		= actions.remove(clickType)
+
+
+	@JvmOverloads
+	fun addAction(clickType: ClickType = LEFT, consumer: Consumer<InventoryClickEvent>) = addAction(clickType) {
+		consumer.accept(this)
+	}
 
 }
