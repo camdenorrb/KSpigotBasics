f60cbf54f1505783badd07368d5d1bc483159b6c
camdenorrb <camdenorrb@users.noreply.github.com>
2017-12-29 05:29:54 -0600

    Meow

---
+++ src/main/kotlin/me/camdenorrb/kspigotbasics/gson/UUIDAdapt.kt
@@ -0,0 +1,18 @@
+package me.camdenorrb.kspigotbasics.gson
+
+import com.google.gson.*
+import org.bukkit.Location
+import java.lang.reflect.Type
+import java.util.*
+
+class UUIDAdapt : JsonSerializer<UUID>, JsonDeserializer<UUID> {
+
+	override fun serialize(uuid: UUID, p1: Type?, p2: JsonSerializationContext?): JsonElement {
+		return JsonPrimitive(uuid.toString())
+	}
+
+	override fun deserialize(element: JsonElement, p1: Type?, p2: JsonDeserializationContext?): UUID {
+		return UUID.fromString(element.asString)
+	}
+
+}
